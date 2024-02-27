33dd5d5457de4bfa4006d73bbac56c53340c8a86
markhughes321 <markhughes321@gmail.com>
2024-02-27 21:56:23 +0000

    added contact form and updated sitemap

--- dist/js/submit-form.js
+++ dist/js/submit-form.js
@@ -1,28 +1,35 @@
 document.getElementById("contact-form").addEventListener("submit", async function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const formData = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value
-  };
+    const formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        company: document.getElementById("company").value
+    };
 
-  try {
-      const response = await fetch('https://form-handler-markhughesqa.netlify.app/.netlify/functions/submit-form', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData)
-      });
+    try {
+        const response = await fetch('https://form-handler-markhughesqa.netlify.app/.netlify/functions/submit-form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        });
 
-      if (response.ok) {
-          alert("Message sent successfully!");
-      } else {
-          alert("Error sending message. Please try again.");
-      }
-  } catch (error) {
-      console.error("Error:", error);
-      alert("Error sending message. Please try again.");
-  }
+        const responseData = await response.json(); // Parse JSON response
+
+        if (response.ok) {
+            document.getElementById("contact-form").reset(); // Clear form fields
+            document.getElementById("form-message").innerText = responseData.message; // Display success message
+            setTimeout(() => {
+                document.getElementById("form-message").innerText = ''; // Clear success message after 5 seconds
+            }, 5000);
+        } else {
+            document.getElementById("form-message").innerText = "Error sending message. Please try again."; // Display error message
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        document.getElementById("form-message").innerText = "Error sending message. Please try again."; // Display error message
+    }
 });
